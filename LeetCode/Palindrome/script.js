c12bfa35cc53e916d216e795f8da5660c66320c8
AgusFerrando <104893201+AgusFerrando@users.noreply.github.com>
2022-06-16 00:26:24 +0200

    mas ejercicios

---
+++ LeetCode/Palindrome/script.js
@@ -0,0 +1,16 @@
+var isPalindrome = function(x) {
+    let num = x.toString();
+    let numInverso = "";
+    let numLength = num.length
+   
+    for (let i = 0; i < numLength; i++) {
+       
+        let element = num.charAt(numLength-1-i);
+        numInverso += element
+       
+    }
+     
+    if (num == numInverso){
+        return true;
+    } else return false;
+};
